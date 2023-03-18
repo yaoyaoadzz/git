0f9f5d1fdb4bb46a39ced065e6164976ce55f923
yaoyao <1152260308@qq.com>
2023-03-18 23:16:05 +0800

    first commit

---
+++ src/main/java/com/yaoyao/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.yaoyao.git;
+
+/**
+ * @author yaoyao
+ * @date 2023/3/18 -23:08
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
